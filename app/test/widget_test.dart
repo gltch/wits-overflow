bd641ea2ec13d3cd1a121742a43803a4d7f495e4
thefirsthero <kingvukosi@outlook.com>
2021-04-22 11:21:32 +0200

    Added tests for post questions UI

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -5,26 +5,42 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
+import 'dart:math';
 
-// import 'package:wits_overflow/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:wits_overflow/main.dart';
 
 void main() {
-//   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-//     // Build our app and trigger a frame.
-//     await tester.pumpWidget(MyApp());
+  testWidgets(
+      'Enter title and question body, then press post and cclear fields',
+      (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
 
-//     // Verify that our counter starts at 0.
-//     expect(find.text('0'), findsOneWidget);
-//     expect(find.text('1'), findsNothing);
+    // Finds text fields
+    var textFieldTitle = find.byKey(ValueKey(1));
+    expect(textFieldTitle, findsOneWidget);
 
-//     // Tap the '+' icon and trigger a frame.
-//     await tester.tap(find.byIcon(Icons.add));
-//     await tester.pump();
+    var textFieldBody = find.byKey(ValueKey(2));
+    expect(textFieldBody, findsOneWidget);
 
-//     // Verify that our counter has incremented.
-//     expect(find.text('0'), findsNothing);
-//     expect(find.text('1'), findsOneWidget);
-//   });
+    // Enters text into textfields
+    await tester.enterText(textFieldTitle, 'Test Title');
+    expect(find.text('Test Title'), findsOneWidget);
+
+    await tester.enterText(textFieldBody, 'Test Question Body');
+    expect(find.text('Test Question Body'), findsOneWidget);
+
+    // Find post button
+    var button = find.byIcon(Icons.add);
+    expect(button, findsOneWidget);
+
+    // Tap post button and expect cleared textfields
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+    expect(find.text('Test Title'), findsNothing);
+    expect(find.text('Test Question Body'), findsNothing);
+  });
 }
