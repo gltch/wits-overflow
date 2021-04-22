492832d4563c4b5008a8e874a5a3e95441740e51
thefirsthero <kingvukosi@outlook.com>
2021-04-22 11:22:48 +0200

    Corrected spelling error

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -13,8 +13,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:wits_overflow/main.dart';
 
 void main() {
-  testWidgets(
-      'Enter title and question body, then press post and cclear fields',
+  testWidgets('Enter title and question body, then press post and clear fields',
       (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
