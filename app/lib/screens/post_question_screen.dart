2e63d1905ae6a4dcfe1411515f06d782eda24758
thefirsthero <kingvukosi@outlook.com>
2021-05-08 22:16:51 +0200

    Added basic unfinished sidebar to these screens

--- app/lib/screens/post_question_screen.dart
+++ app/lib/screens/post_question_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wits_overflow/utils/sidebar.dart';
 import 'package:wits_overflow/utils/wits_overflow_api.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
@@ -97,6 +98,7 @@ class _PostQuestionState extends State<PostQuestionScreen> {
           }
         },
         child: Scaffold(
+          drawer: SideDrawer(),
           appBar: AppBar(
             // Here we take the value from the MyPostQuestionsPage object that was created by
             // the App.build method, and use it to set our appbar title.
