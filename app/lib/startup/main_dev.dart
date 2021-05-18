16d591ed807458e26f12e82108cb77503dba7215
motsoaledi matlala <reuben.mahloele@gmail.com>
2021-05-18 13:39:16 +0200

    just a commit

--- app/lib/startup/main_dev.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
-import 'package:wits_overflow/startup/wits_overflow_app.dart';
-
-Future<void> main() async {
-  await DotEnv.load(fileName: "env/.env_dev");
-  runApp(WitsOverflowApp());
-}
