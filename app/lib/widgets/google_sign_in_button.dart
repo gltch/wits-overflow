ba92f70ea5feeb4cc3a204716e6e8bb11824b53a
thefirsthero <kingvukosi@outlook.com>
2021-05-23 18:33:23 +0200

    Removed unused imports

--- app/lib/widgets/google_sign_in_button.dart
+++ app/lib/widgets/google_sign_in_button.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:wits_overflow/screens/api_request_example.dart';
 import 'package:wits_overflow/screens/home_screen.dart';
 //import 'package:wits_overflow/screens/home_screen.dart';
 //import 'package:wits_overflow/screens/user_info_screen.dart';
