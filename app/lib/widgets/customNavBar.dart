915cc25f2638a9b213a7b80f3f898b8feccdbd05
Thabiso kobe <kobethabiso360@gmail.com>
2021-05-23 16:27:34 +0200

    removed customNavBar

--- app/lib/widgets/customNavBar.dart
+++ app/lib/widgets/customNavBar.dart
@@ -4,46 +4,8 @@ class CustomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      //TODO Add your code for the side bar in here
+        //TODO Add your code for the side bar in here
 
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          UserAccountsDrawerHeader(
-            accountName: Text("Username"),
-            accountEmail: Text("Example_mail@gmail.com"),
-            currentAccountPicture: CircleAvatar(
-              child: ClipOval(
-                child: Image.asset(
-                  'assets/images/profilepic.jpg',
-                  width: 80,
-                  height: 80,
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            decoration: BoxDecoration(
-              color: Colors.blueAccent,
-              image: DecorationImage(
-                image: AssetImage('assets/images/backround.png'),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          ListTile(
-              leading: Icon(Icons.portrait_rounded),
-              title: Text('User details'),
-              onTap: () => null),
-          ListTile(
-              leading: Icon(Icons.favorite),
-              title: Text('Favourites'),
-              onTap: () => null),
-          ListTile(
-              leading: Icon(Icons.logout),
-              title: Text('Log out'),
-              onTap: () => null),
-        ],
-      ),
-    );
+        );
   }
 }
