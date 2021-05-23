55b99eb52ce05e586e2d6ea50e4e999e8d188843
thefirsthero <kingvukosi@outlook.com>
2021-05-23 17:14:40 +0200

    Updated API to allow for questions filtering

--- app/lib/utils/wits_overflow_api.dart
+++ app/lib/utils/wits_overflow_api.dart
@@ -6,14 +6,18 @@ import 'package:wits_overflow/utils/storage.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class WitsOverflowApi {
-  static Future<List<Question>> fetchQuestions() async {
+  static Future<List<Question>> fetchQuestions(String moduleId) async {
     // Get the list of questions from the api
     final token = await SecureStorage.read('user.token');
 
     // Get the api url from the environmental variables
     String apiBaseUrl = env['API_BASE_URL'].toString();
 
-    final response = await http.get(Uri.parse("$apiBaseUrl/questions"),
+    // Format query parameters
+    var queryParameters = {'module': moduleId};
+
+    final response = await http.get(
+        Uri.http("$apiBaseUrl", "/questions", queryParameters),
         headers: {'token': token ?? ''});
 
     if (response.statusCode == 200) {
