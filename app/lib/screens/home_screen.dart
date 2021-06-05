de1a8c6f8263770f9c851c2996758242644abb2d
Anton Stott <antonstott@gmail.com>
2021-06-05 11:33:20 +0200

    Empty

--- app/lib/screens/home_screen.dart
+++ app/lib/screens/home_screen.dart
@@ -2,48 +2,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:wits_overflow/utils/sidebar.dart';
 
-// -----------------------------------------------------------------------------
-//             Dashboard class
-// -----------------------------------------------------------------------------
 class HomeScreen extends StatefulWidget {
-  // final logoutAction;
-
-  final state = HomeScreenState(CircularProgressIndicator());
-
-  HomeScreen() {
-    print('[DASHBOARD CONSTRUCTOR]');
-  }
-
   @override
-  HomeScreenState createState() => state;
+  HomeScreenState createState() => HomeScreenState();
 }
 
-// -----------------------------------------------------------------------------
-//    DASHBOARD STATE CLASS
-// -----------------------------------------------------------------------------
 class HomeScreenState extends State<HomeScreen> {
-  Widget child;
-
-  HomeScreenState(this.child);
 
   @override
   Widget build(BuildContext context) {
-    print('[DASHBOARD STATE]');
-    // CollectionReference questions =
-    //     FirebaseFirestore.instance.collection('questions');
-    return Scaffold(
-      drawer: SideDrawer(),
-      backgroundColor: Colors.white,
-      appBar: AppBar(elevation: 0, backgroundColor: Colors.blue),
-      body: Center(child: Text("Wits Overflow Home Screen")),
+    return Center(
+      child: Text('Wits Overflow')
     );
   }
 
-  void show(Widget widget) {
-    this.setState(() {
-      child = widget;
-    });
-  }
 }
