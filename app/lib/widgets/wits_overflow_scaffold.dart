412d923343afd07113f46b12d0472e019f733251
Anton Stott <antonstott@gmail.com>
2021-06-06 13:19:17 +0200

    Post question page working

--- app/lib/widgets/wits_overflow_scaffold.dart
+++ app/lib/widgets/wits_overflow_scaffold.dart
@@ -7,7 +7,7 @@ class WitsOverflowScaffold extends StatelessWidget {
 
   Future<List<Map<String, dynamic>>> _courses;
   Future<List<Map<String, dynamic>>> _modules;
-  FloatingActionButton _floatingActionButton;
+  FloatingActionButton? _floatingActionButton;
   final Widget body;
 
   WitsOverflowScaffold({required this.body, courses, modules, floatingActionButton}) :
@@ -18,7 +18,8 @@ class WitsOverflowScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
+    if (this._floatingActionButton != null) {
+return Scaffold(
       floatingActionButton: this._floatingActionButton,
       appBar: AppBar(
         title: const Text('Wits Overflow'),
@@ -37,6 +38,28 @@ class WitsOverflowScaffold extends StatelessWidget {
       drawer: SideDrawer(courses: this._courses, modules: this._modules),
       body: this.body
     );
+    } else {
+      return Scaffold(
+      appBar: AppBar(
+        title: const Text('Wits Overflow'),
+        actions: [
+          // IconButton(
+            //   icon: Image.network(
+            //     FirebaseAuth.instance.currentUser!.photoURL!
+            //   ), 
+            //   onPressed: () {
+            //     ScaffoldMessenger.of(context).showSnackBar(
+            //         const SnackBar(content: Text('This is a snackbar')));
+            //   }),
+              VerticalDivider(color: Colors.transparent,),
+          ]
+        ),
+      drawer: SideDrawer(courses: this._courses, modules: this._modules),
+      body: this.body
+    );
+    }
+
+    
 
   }
 
