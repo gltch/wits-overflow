b49cca84efc3add911ec818515a77a6a0bb065f4
Anton Stott <antonstott@gmail.com>
2021-06-07 13:26:59 +0200

    Analysis fixed

--- app/lib/screens/post_question_screen.dart
+++ app/lib/screens/post_question_screen.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:wits_overflow/screens/question_and_answers_screen.dart';
 import 'package:wits_overflow/utils/wits_overflow_data.dart';
 import 'package:wits_overflow/widgets/wits_overflow_scaffold.dart';
